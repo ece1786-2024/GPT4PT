24c19e12468862d3f2f2413645a0d029aff2c574
Bryan Zhu <bryan-zhu@hotmail.com>
2024-11-24 23:24:09 -0500

    add main (#3)

---
+++ main.py
@@ -0,0 +1,15 @@
+from sn_agent import SnClassifier
+
+# Introvert (I) and Extrovert (E)
+# c1 = IeClassifier().start()
+
+# Sensing (S) and Intuition (N)
+c2 = SnClassifier().start()
+
+# Thinking (T) and Feeling (F)
+#c3 = TfClassifier().start()
+
+# Judging (J) and Perceiving (P)
+#c4 = JpClassifier().start()
+
+#print(f'Final prediction: {c1}{c2}{c3}{c4}')
